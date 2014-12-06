47b881b7686be0e02fab45e0b53456e336ea0170
lukaszotto <lukasz.otto@yahoo.com>
2014-12-06 18:03:14 +0100

    simple routing

--- js/app.js
+++ js/app.js
@@ -13,19 +13,31 @@ formApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/form',
             templateUrl: 'form.html',
             controller: 'formController'
-        });
+        })
         
         // nested states 
         // each of these sections will have their own view
         // url will be nested (/form/profile)
-
+        .state('form.account', {
+            url: '/account',
+            templateUrl: 'form-account.html',
+            controller: 'accountController'
+        })
+        .state('form.delivery', {
+            url: '/delivery',
+            templateUrl: 'form-delivery.html'
+        })
+        .state('form.payment', {
+            url: '/payment',
+            templateUrl: 'form-payment.html'
+        });
     // catch all route
     // send users to the form page 
-})
+});
 
 // our controller for the form
 // =============================================================================
-.controller('formController', function($scope) {
+formApp.controller('formController', function($scope) {
     
     // we will store all of our form data in this object
     $scope.formData = {};
@@ -36,3 +48,13 @@ formApp.config(function($stateProvider, $urlRouterProvider) {
     };
     
 });
+formApp.controller('accountController', function($scope) {
+    
+    // we will store all of our form data in this object
+    $scope.formAcountData = {};
+    
+    // function to process the form
+
+    
+});
+
